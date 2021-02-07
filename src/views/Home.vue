d93092ee66541759a66dbefd733fb9f0eeb9b014
robertobirijnr <robertobirijnr@hotmail.com>
2021-02-07 00:32:51 +0000

    setting up tail wind css with client app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div
+      class="flex items-center justify-center mt-48 lg:mt-0 lg:min-h-screen text-center"
+    >
+      <div class="flex-initial">
+        <img
+          class="object-contain self-center mx-auto h-32"
+          alt="Vue logo"
+          src="@/assets/logo.png"
+        />
+        <HelloWorld class="w-full" msg="Welcome to Your Vue.js App" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style></style>
