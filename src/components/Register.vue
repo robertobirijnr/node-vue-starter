8d648f13f2733dc0db9ca0a0bde8a0469fc18e06
robertobirijnr <robertobirijnr@hotmail.com>
2021-02-08 17:44:28 +0000

    register page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="container w-full h-12 bg-red-700 mx-auto">
-dsdsdsds
+    <div class="container w-full my-16 mx-auto">
+        <div class="max-w-xs mx-auto h-12">
+            <h2 class="text-center pt-2 text-gold">Sign up</h2>
+
+            <div class="w-full bg-white shadow mt-5 rounded-sm p-8">
+                <input type="text" class="w-full text-xs focus:outline-none bg-brown-lightest text-brown p-3 mb-3" placeholder="Enter Your Name">
+                <input type="text" class="w-full text-xs focus:outline-none bg-brown-lightest text-brown p-3 mb-3" placeholder="Enter Your Email">
+                <input type="text" class="w-full text-xs focus:outline-none bg-brown-lightest text-brown p-3" placeholder="Enter Your Password">
+
+                <button class="w-full mt-3 py-4 bg-emerald-100 text-white rounded-sm focus:outline-none hover:bg-emerald-500">
+                    Send
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
